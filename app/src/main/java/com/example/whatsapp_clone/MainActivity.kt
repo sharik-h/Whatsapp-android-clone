02fe2be675b5961b27a64bd6c9f5f9a4a1a5d4f3
Mohammed sharikh <mohammedsharik0123@gmail.com>
2022-06-21 11:27:36 +0530

    Added options button and dropdown menu

--- app/src/main/java/com/example/whatsapp_clone/MainActivity.kt
+++ app/src/main/java/com/example/whatsapp_clone/MainActivity.kt
@@ -1,32 +1,18 @@
 package com.example.whatsapp_clone
 
-import android.app.ActionBar
 import android.os.Bundle
-import android.widget.TabHost
-import android.widget.TableRow
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.indication
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Tab
-import androidx.compose.material.TabRow
-import androidx.compose.material.TabRowDefaults
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.*
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -52,20 +38,94 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TabSample() {
     var selectedIndex by remember { mutableStateOf(1) }
-    var pagerState = rememberPagerState()
+    var pagerState = rememberPagerState(1)
     val couroutineScope = rememberCoroutineScope()
+    var expanded1 by remember { mutableStateOf(false)}
+    var expanded2 by remember { mutableStateOf(false)}
+    var expanded3 by remember { mutableStateOf(false)}
     val tabsList = listOf("CAMERA", "CHATS", "STATUS", "CALLS")
     val cameraImg = painterResource(id = R.drawable.camera_img)
+    val optionsImg = painterResource(id = R.drawable.option_img)
+    val searchImg = painterResource(id = R.drawable.search_img)
+
 
     Column(Modifier.fillMaxSize()) {
         TopAppBar(
             modifier = Modifier.fillMaxWidth(),
             backgroundColor = Color(0xFF00827f),
-            contentPadding = PaddingValues(0.dp),
         ) {
-            Text(text = "Whatsapp", fontSize = 20.sp, color = Color.White, modifier = Modifier.padding(10.dp))
+            Text(
+                text = "Whatsapp",
+                fontSize = 20.sp,
+                color = Color.White,
+                modifier = Modifier.padding(10.dp)
+            )
+            Spacer(modifier = Modifier.weight(0.5f))
+            IconButton(onClick = { /*TODO*/ }) {
+                Image(painter = searchImg, contentDescription = "")
+            }
+            IconButton(onClick = {
+                when(pagerState.currentPage) {
+                    1 -> expanded1 = true
+                    2 -> expanded2 = true
+                    3 -> expanded3 = true
+                }
+
+            }) {
+                DropdownMenu(
+                    expanded = expanded1,
+                    onDismissRequest = { expanded1 = false },
+                    Modifier.width(180.dp)
+                ) {
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "New group")
+                    }
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "New broadcast")
+                    }
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "Linked devices")
+                    }
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "Starred messages")
+                    }
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "Payments")
+                    }
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "Settings")
+                    }
+                }
+                DropdownMenu(
+                    expanded = expanded2,
+                    onDismissRequest = { expanded2 = false },
+                    Modifier.width(180.dp)
+
+                ) {
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "Status privacy")
+                    }
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "Settings")
+                    }
+                }
+                DropdownMenu(
+                    expanded = expanded3,
+                    onDismissRequest = { expanded3 = false },
+                    Modifier.width(180.dp)
+                ) {
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "Clear call log")
+                    }
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "Settings")
+                    }
+                }
+                Image(painter = optionsImg, contentDescription = "")
+            }
         }
-        TabRow(selectedTabIndex = selectedIndex,
+        TabRow(
+            selectedTabIndex = selectedIndex,
             backgroundColor = Color(0xFF00827f),
             indicator = {  tabPosition ->
                 TabRowDefaults.Indicator(
